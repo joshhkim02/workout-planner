c70fc1572177adc1987cb29ffc72228239456774
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2025-03-19 16:23:14 -0700

    feat: user route/controller initialization

---
+++ backend/controllers/userController.js
@@ -0,0 +1,4 @@
+const registerController = async (req, res) => {};
+const loginController = async (req, res) => {};
+const logoutController = async (req, res) => {};
+const getUserController = async (req, res) => {};
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const {
+  registerController,
+  loginController,
+  logoutController,
+  getUserController,
+} = require('../controllers/userController');
+
+router.post('/register', registerController);
+router.post('/login', loginController);
+router.post('/logout', logoutController);
+router.get('/user', getUserController);
+
+module.exports = router;
