bc948fe0d3d6430839ac52c8296f15975ff66896
joshhkim02 <joshhkim02@gmail.com>
2025-03-25 22:55:21 -0700

    feat: frontend initialization

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config({ path: '../.env' })
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 const userRoutes = require('./routes/userRoutes');
 const workoutRoutes = require('./routes/workoutRoutes');
 const exerciseRoutes = require('./routes/exerciseRoutes');
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
